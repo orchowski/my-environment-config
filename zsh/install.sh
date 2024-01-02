7bdf8ac9a6b2e064e9aeef28f96e81ec1d17f46d
Aleksander Orchowski <orchowskialeksander@gmail.com>
2024-01-02 22:26:59 +0100

    neorg, nvim, zsh tweaks

---
+++ zsh/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+
