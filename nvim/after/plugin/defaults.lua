f18fa37a4e51a599fea4d48b16147bfd49997711
Aleksander Orchowski <orchowskialeksander@gmail.com>
2025-02-21 21:33:31 +0100

    nvim update

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -1,1 +1,8 @@
 vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<leader>:', ':vsplit<CR>', { desc = 'vertical split - new window' })
+vim.keymap.set('n', '<leader>%', ':split<CR>', { desc = 'horizontal split - new window' })
+vim.keymap.set('n', '<leader>pp', function()
+	vim.fn.setreg('+', vim.fn.expand('%'))
+end
+)
